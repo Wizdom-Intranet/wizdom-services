a6ed6674e714c8c29ea8021086fff93eba04d871
David Sørensen <david.kaae@gmail.com>
2020-03-18 14:43:43 +0100

    Changing to await instead of then

--- src/services/wizdom.language.update.ts
+++ src/services/wizdom.language.update.ts
@@ -9,23 +9,20 @@ export class WizdomLanguageUpdate {
     public async UpdateIfNeededAsync(webAbsoluteUrl: string, currentUserLanguage: string) { 
         await this.cache.Localstorage.ExecuteCached("WizdomUserLanguage", async () => {            
             var apiUrl ="/_api/SP.UserProfiles.PeopleManager/GetMyProperties";
-            return this.spHttpClient.get(webAbsoluteUrl + apiUrl)
-            .then((response): Promise<string> => {          
-                return response.json();
-            }).then((result: any) => {
-                var preferredLanguage:string = "";
-                (result.UserProfileProperties).forEach(async property => {
-                    if(property.Key == "SPS-MUILanguages" && property.Value)
-                    {
-                        preferredLanguage = property.Value.split(',')[0];                        
-                        if(preferredLanguage.toLowerCase() !== currentUserLanguage.toLowerCase()){
-                            this.wizdomWebApiService.Put("api/wizdom/365/principals/me/language", preferredLanguage);                        
-                        }
-                        return; // break loop
+            var response = await this.spHttpClient.get(webAbsoluteUrl + apiUrl);            
+            var result:any = await response.json();
+            var preferredLanguage:string = "";
+            (result.UserProfileProperties).forEach(async property => {
+                if(property.Key == "SPS-MUILanguages" && property.Value)
+                {
+                    preferredLanguage = property.Value.split(',')[0];                        
+                    if(preferredLanguage.toLowerCase() !== currentUserLanguage.toLowerCase()){
+                        this.wizdomWebApiService.Put("api/wizdom/365/principals/me/language", preferredLanguage);                        
                     }
-                });
-                return preferredLanguage;                
+                    return; // break loop
+                }
             });
+            return preferredLanguage;                
         }, 
         1000 * 60 * 60 * 24 * 30, // expire in 30 days
         1000 * 60 * 60 * 12, // refresh every 12 hour
