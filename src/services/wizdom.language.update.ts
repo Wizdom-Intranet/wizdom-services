c2a2cd472afe4de7cdc7135ba5d76a0458690d59
Bo Sandorf Pluszek (bsc AT webtop.dk) <bsc@webtop.dk>
2020-11-30 11:16:43 +0100

    Better detection of userlanguage

--- src/services/wizdom.language.update.ts
+++ src/services/wizdom.language.update.ts
@@ -16,20 +16,30 @@ export class WizdomLanguageUpdate {
             (result.UserProfileProperties).forEach(async property => {
                 if(property.Key == "SPS-MUILanguages" && property.Value)
                 {
-                    preferredLanguage = property.Value.split(',')[0];                        
-                    if(preferredLanguage.toLowerCase() !== currentUserLanguage.toLowerCase()){
-                        if(wizdomConfiguration && wizdomConfiguration.Wizdom365 && wizdomConfiguration.Wizdom365.Languages)
-                        {
-                            for(var i=0; i<wizdomConfiguration.Wizdom365.Languages.length; i++){
-                                if(wizdomConfiguration.Wizdom365.Languages[i].Tag.toLowerCase() == preferredLanguage.toLowerCase())
+                    // debugger;
+                    // console.log("Current user ProfileProperties languages:", property.Value);
+                    // console.log("Wizdom languages:", wizdomConfiguration.Wizdom365.Languages);
+                    // console.log("Current registered userlanguage:", currentUserLanguage);
+
+                    var profileLanguages = property.Value.toLowerCase().split(',');
+                    for (var profileLanguagesI = 0; profileLanguagesI < profileLanguages.length; profileLanguagesI++) {
+                        // check if its a valid selected wizdom language
+                        for (var wizdomLanguageI = 0; wizdomLanguageI < wizdomConfiguration.Wizdom365.Languages.length; wizdomLanguageI++) {
+                            if (wizdomConfiguration.Wizdom365.Languages[wizdomLanguageI].Tag.toLowerCase().indexOf(profileLanguages[profileLanguagesI]) == 0) {
+                                // found a match! lets update the principal if needed
+                                // console.log("found language match", profileLanguages[profileLanguagesI], wizdomConfiguration.Wizdom365.Languages[wizdomLanguageI].Tag.toLowerCase());
+                                if (currentUserLanguage != wizdomConfiguration.Wizdom365.Languages[wizdomLanguageI].Tag.toLowerCase()) //note, we're doing the comparison to the wizdom langauge, as the profile language might just be "en" instead of "en-us"
                                 {
-                                    this.wizdomWebApiService.Put("api/wizdom/365/principals/me/language", preferredLanguage); 
-                                    break;
+                                    // console.log("updating wizdom principal");
+                                    this.wizdomWebApiService.Put("api/wizdom/365/principals/me/language", wizdomConfiguration.Wizdom365.Languages[wizdomLanguageI].Tag.toLowerCase()); 
                                 }
-                            }                            
-                        }                                                   
+                                else {
+                                    // console.log("wizdom principal already uptodate");
+                                }
+                                return; // break out of all the for
+                            }
+                        }
                     }
-                    return; // break loop
                 }
             });
             return preferredLanguage;                
